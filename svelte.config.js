21e180d0f81aba711e96ddaa79fc92d11a3ebf94
TizFox <tizbona04@gmail.com>
2026-04-13 21:00:47 +0200

    fixed adapter: from -auto to -static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,11 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: "404.html",
+		}),
 		paths: {
-			base: process.argv.includes("dev")
-				? ""
-				: "https://tizfox.github.io/DnD_CharacterSheet/",
+			base: "/DnD_Forge",
 		},
 	},
 };
