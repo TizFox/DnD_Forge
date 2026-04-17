7da8683d800a4fbaca85643f710de1c013be3d3d
TizFox <tizbona04@gmail.com>
2026-04-17 21:59:51 +0200

    navbar now stay put at (0,0)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,63 +34,59 @@
 
 <!------------------------------------------>
 
-<nav class="navbar-container">
-	<div class="navbar">
-		<img src={logo} alt="Logo" class="h-full" />
-		<div class="user-actions">
-			<button
-				onclick={() => {
-					dndChar = new Character();
-				}}
-				class="std-btn">New Character</button
-			>
+<nav class="bar-container navbar">
+	<img src={logo} alt="Logo" class="h-full" />
+	<div class="user-actions">
+		<button
+			onclick={() => {
+				dndChar = new Character();
+			}}
+			class="std-btn">New Character</button
+		>
 
-			<button
-				onclick={() => {
-					if (dndChar) {
-						save(dndChar);
-					}
-				}}
-				class="std-btn">Save</button
-			>
+		<button
+			onclick={() => {
+				if (dndChar) {
+					save(dndChar);
+				}
+			}}
+			class="std-btn">Save</button
+		>
 
-			<!-- Input -->
-			<div class="w-fit relative group rounded-lg">
-				<button
-					type="button"
-					onclick={() => fileInput?.click()}
-					class="w-full h-full px-11 py-3 bg-z2 rounded-lg
+		<!-- Input -->
+		<div class="w-fit relative group rounded-lg">
+			<button
+				type="button"
+				onclick={() => fileInput?.click()}
+				class="w-full h-full px-11 py-3 bg-z2 rounded-lg
 						border-2 border-dark transition-std
 						hover:border-cta
 						base-text text-left truncate
 						{fileName ? 'text-dark' : 'text-dark/25'}"
-				>
-					<FileUser
-						class="h-1/2 absolute top-1/4 left-3 transition-std text-dark group-hover:text-cta"
-					/>
-					{fileName ?? "Character FIle"}
-				</button>
-				<input
-					bind:this={fileInput}
-					oninput={() => {
-						if (fileInput?.files && fileInput.files.length > 0) {
-							fileName = fileInput.files[0].name;
-							load(fileInput.files[0], (data: CharacterType) => {
-								dndChar = Character.from(data);
-							});
-						}
-					}}
-					class="hidden"
-					type="file"
-					accept={FILE_EXTENTION}
-					multiple={false}
+			>
+				<FileUser
+					class="h-1/2 absolute top-1/4 left-3 transition-std text-dark group-hover:text-cta"
 				/>
-			</div>
+				{fileName ?? "Character FIle"}
+			</button>
+			<input
+				bind:this={fileInput}
+				oninput={() => {
+					if (fileInput?.files && fileInput.files.length > 0) {
+						fileName = fileInput.files[0].name;
+						load(fileInput.files[0], (data: CharacterType) => {
+							dndChar = Character.from(data);
+						});
+					}
+				}}
+				class="hidden"
+				type="file"
+				accept={FILE_EXTENTION}
+				multiple={false}
+			/>
 		</div>
 	</div>
 </nav>
-
-<section class="divider"></section>
 
 <main class="flex items-center justify-center">
 	<div class="w-full max-w-7xl min-h-(--main-size) p-5 flex flex-col gap-5">
@@ -114,21 +110,15 @@
 	</div>
 </main>
 
-<footer class="footer-container">
-	<div class="footer">
-		<img src={logo} alt="Logo" class="h-full" />
-		<div>
-			<h1>{NAME}</h1>
-			<p>
-				By <a
-					target="_blank"
-					href="https://github.com/TizFox"
-					class="link"
-				>
-					TizFox
-				</a>
-			</p>
-		</div>
+<footer class="bar-container footer">
+	<img src={logo} alt="Logo" class="h-full" />
+	<div>
+		<h1>{NAME}</h1>
+		<p>
+			By <a target="_blank" href="https://github.com/TizFox" class="link">
+				TizFox
+			</a>
+		</p>
 	</div>
 </footer>
 
@@ -137,31 +127,21 @@
 <style lang="postcss">
 	@import "$lib/theme.css";
 
-	.navbar-container {
-		@apply w-full h-(--bars-size) z-10
-		fixed top-0 left-0;
-	}
-	.navbar {
-		@apply w-full h-full p-5 bg-z2
-		flex flex-row items-center justify-start gap-5
+	.bar-container {
+		@apply w-full h-(--bars-size) p-5 bg-z2
+		flex flex-row items-center gap-5
 		shadow-2xl;
 
-		.user-actions {
-			@apply w-full h-full
-			flex flex-row items-center justify-end gap-5;
+		&.navbar {
+			@apply justify-start;
+			.user-actions {
+				@apply w-full h-full
+				flex flex-row items-center justify-end gap-5;
+			}
 		}
-	}
 
-	.divider {
-		@apply h-(--bars-size);
-	}
-
-	.footer-container {
-		@apply w-full h-(--bars-size) z-10;
-	}
-	.footer {
-		@apply w-full h-full p-5 bg-z2
-		flex flex-row items-center justify-center gap-5
-		shadow-2xl;
+		&.footer {
+			@apply justify-center;
+		}
 	}
 </style>
